bf93dc158adcf3b5f9b3b91984215f7172a5ffdd
anjukrishna07 <117815090+anjukrishna07@users.noreply.github.com>
2023-04-05 12:01:34 +0530

    Add files via upload

---
+++ arithemetic.sh
@@ -0,0 +1,14 @@
+echo Enter the number1
+ read a
+ echo Enter the number2
+ read b
+ c=`expr $a + $b`
+ echo Addition= $c
+ d=`expr $a - $b`
+ echo subtraction=$d
+ e=`expr $a \* $b`
+ echo Multipliccation=$e
+ f=`expr $a / $b`
+ echo Division=$f
+
+
